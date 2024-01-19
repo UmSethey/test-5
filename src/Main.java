40a301326cf64134ae45c11c5bd68bc439f50300
UmSethey <setheyum5050@gmail.com>
2024-01-19 19:55:50 +0700

    first commit23

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing
+        //efjhjhjhjhjh
     }
 }
